fa17ca6e46be71b6bbebabcb4070d64c769a1d3b
ankhoa18599 <44254391+ankhoa18599@users.noreply.github.com>
2018-10-18 14:20:46 +0700

    Update Name.cpp

--- Name.cpp
+++ Name.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
 	cout<<"My name is:"<<endl;
-	cout<<"*  *  *  *  ****     *"<<endl;
-	cout<<"* *   *  *  *  *    * *"<<endl;
-	cout<<"**    ****  *  *   *****"<<endl;
-	cout<<"* *   *  *  *  *  *     *"<<endl;
-	cout<<"*  *  *  *  **** *       *"<<endl;	
+	cout<<"**  **  **  **  ******     **"<<endl;
+	cout<<"** **   **  **  **  **    ** **"<<endl;
+	cout<<"****    ******  **  **   *******"<<endl;
+	cout<<"** **   **  **  **  **  **     **"<<endl;
+	cout<<"**  **  **  **  ****** **       **"<<endl;	
 	return 0;
 }
 
